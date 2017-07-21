63d7bd3e68c6d41246f6643455b752784ba60467
Mendel Konikov <mkonikov@gmail.com>
2017-07-21 16:40:04 -0400

    Add searchable module

--- ActiveRecordLite/lib/02_searchable.rb
+++ ActiveRecordLite/lib/02_searchable.rb
@@ -4,11 +4,10 @@ require 'byebug'
 
 module Searchable
   def where(params)
-    where_line = params.map { |k,_| "#{k} = ?" }
-    where_line = where_line.join(" AND ")
+    where_line = params.map { |k,_| "#{k} = ?" }.join(" AND ")
     values = params.map { |_,v| v }
 
-    DBConnection.execute(<<-SQL, *values)
+    data = DBConnection.execute(<<-SQL, *values)
       SELECT
         *
       FROM
@@ -16,6 +15,7 @@ module Searchable
       WHERE
         #{where_line}
     SQL
+    # self.parse_all(data)
 
   end
 end

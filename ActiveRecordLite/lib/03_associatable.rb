a46cd9cd078c8b4440c6e78663618b2ba23b4039
Mendel Konikov <mkonikov@gmail.com>
2017-07-21 17:35:20 -0400

    Create associations class

--- ActiveRecordLite/lib/03_associatable.rb
+++ ActiveRecordLite/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,29 +11,39 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".downcase.to_sym
+    @primary_key = options[:primary_key] || "id".to_sym
+    @class_name = options[:class_name] || "#{name}".camelcase.singularize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".downcase.to_sym
+    @primary_key = options[:primary_key] || "id".to_sym
+    @class_name = options[:class_name] || "#{name}".camelcase.singularize
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    options = BelongsToOptions.new(name, options)
+
+    # debugger
+    name.send(define_method, :foreign_key) { options.foreign_key }
+    name.send(define_method, :primary_key) { options.primary_key }
+    name.send(define_method, :class_name) { options.class_name }
     # ...
   end
 
@@ -47,4 +58,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
